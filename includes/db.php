0989a4f75b29768c0e923c6d703e0d333a130c43
Kawya Dissanayaka <kawyadissanayaka95@gmail.com>
2025-09-03 00:55:51 +0530

    signup.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,3 @@
-<?php
-$host = "localhost";
-$user = "root";
 $pass = "";
 $db   = "shopnest";
 
@@ -8,5 +5,3 @@ $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-?>
